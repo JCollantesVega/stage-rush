f3b06e7c9bea049ae42dad483da0d4100962eeab
JCollantesVega <collantes.jantonio@gmail.com>
2025-11-29 01:14:42 +0100

    Fixed CalculateTorque() method

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -6,48 +6,46 @@ public class CarController : MonoBehaviour
     public static CarController Instance { get; private set; }
 
     [Header("References")]
-    public CarFX carFX;
+    public CarFX carFX; //referencia al objeto que maneja los efectos del coche
 
     [Header("DEBUG UI")]
-    public float currentHp;
+    public float currentHp; //caballos de fuerza que da el coche en función de las RPM
 
     [Header("Car Values")]
-    public float brakeForce;
-    public AnimationCurve steeringCurve;
-    [SerializeField] public DriveType driveType;
+    public float brakeForce; //fuerza de frenado aplicada
+    public AnimationCurve steeringCurve; //curva de giro según velocidad
+    [SerializeField] public DriveType driveType; //tipo de tracción(delantera, trasera o 4 ruedas)
 
     [Header("Engine Config")]
-    public float RPM;
-    public float redLine;
-    public float idleRPM;
-    public int currentGear;
-    public float[] gearRatios;
-    public float differentialRatio;
-    public float clutch;
-    public AnimationCurve hpGraph;
-    public GearState gearState;
-    public float increaseGearRPM;
-    public float decreaseGearRPM;
-    public float changeGearTime = 0.5f;
-    public int isEngineRunning;
-    public float wheelRPM;
+    public float RPM; //revoluciones por minuto del motor
+    public float redLine; //revoluciones máximas del motor
+    public float idleRPM; //revoluciones de ralentí
+    public int currentGear; //marcha actual engranada
+    public float[] gearRatios; //relación de marcha para calcular velocidad
+    public float differentialRatio; //relaciçon de diferencial
+    public float clutch; //embrague
+    public AnimationCurve hpGraph; //curva de potencia por RPM
+    public GearState gearState; //Estado de marcha
+    public float increaseGearRPM; //revoluciones a la que se sube de marcha
+    public float decreaseGearRPM; //revoluciones a la que se baja de marcha
+    public float changeGearTime = 0.5f; //tiempo que se tarada en cambiar de marcha
+    public float wheelRPM; //velocidad a la que giran las ruedas(usado para debug)
 
     [Header("Car Components")]
-    public WheelColliders colliders;
-    public WheelMeshes wheelMeshes;
+    public WheelColliders colliders; //grupo de WheelColliders
+    public WheelMeshes wheelMeshes; //grupo de mallas de rueda(modelo 3d)
 
     [HideInInspector]
-    public WheelParticles wheelParticles;
+    public WheelParticles wheelParticles; //grupo de particulas y efectos de cada rueda
 
     [Header("Input Controllers")]
     [SerializeField]private float gasInput;
     [SerializeField]private float brakeInput;
     [SerializeField]private float steerInput;
 
-    public float speed{ get; private set; }
+    public float speed{ get; private set; } //velocidad del coche en u/s(se escala a m/s)
 
-    private Vector3 centerOfMass;
-    public Rigidbody rb;
+    public Rigidbody rb; //referencia al RigidBody del coche
 
 
     float SpeedAtRedline(float gearRatio, float finalDrive, float wheelRadius, float redLine)
@@ -79,7 +77,6 @@ public class CarController : MonoBehaviour
 
     void FixedUpdate()
     {
-        centerOfMass = rb.centerOfMass;
         speed = rb.linearVelocity.magnitude;
 
         GetInput();
@@ -90,12 +87,13 @@ public class CarController : MonoBehaviour
         carFX.HandleEngineSound(RPM / redLine);
         UpdateWheels();
 
-        Debug.Log($"Wheel RPM: FL:{colliders.FLWheel.rpm} FR:{colliders.FRWheel.rpm} RL:{colliders.RLWheel.rpm} RR:{colliders.RRWheel.rpm}");
+        Debug.Log($"Wheel RPM: FL:{(int)colliders.FLWheel.rpm} FR:{(int)colliders.FRWheel.rpm} RL:{(int)colliders.RLWheel.rpm} RR:{(int)colliders.RRWheel.rpm}");
     }
 
+
+    //toma y procesa inputs obtenidos de la instancia InputManager
     private void GetInput()
     {
-        //gasInput = (gearState == GearState.Changing) ? 0f : InputManager.Instance.Gas;
         gasInput = InputManager.Instance.Gas;
         brakeInput = InputManager.Instance.Brake;
         steerInput = InputManager.Instance.Steer;
@@ -118,58 +116,67 @@ public class CarController : MonoBehaviour
 
     }
 
+
+    //Calcula el torque que se manda a las ruedas
     private float CalculateTorque()
     {
+        //variable que se devuelve
         float torque = 0;
 
+        //si las RPM se encuentran dentro de un rango alrededor de ralentí y está la primera marcha engranada
+        //la marcha será punto muerto
         if (RPM < idleRPM + 200 && gasInput == 0 && currentGear == 0)
         {
             gearState = GearState.Neutral;
         }
 
-        if (gearState == GearState.Running && clutch == 0)
+        if (gearState == GearState.Running && clutch == 0) //si hay una marcha engranada y no se está pulsando embrague
         {
-            if (RPM > increaseGearRPM)
+            //sube o baja de marcha según el caso
+            if (RPM >= increaseGearRPM)
             {
                 StartCoroutine(ChangeGear(1));
             }
-            else if (RPM < decreaseGearRPM)
+            else if (RPM <= decreaseGearRPM)
             {
                 StartCoroutine(ChangeGear(-1));
             }
         }
 
 
-        if(gasInput > 0)
+        if(gasInput > 0) //al acelerar
         {
             if (clutch > 0.1f || gearState == GearState.Neutral)
             {
+                //si se está pulsando el embrague o está en punto muerto, las RPM suben sin aplicar fuerza a als ruedas
                 RPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM, redLine * gasInput) + Random.Range(-50, 50), Time.deltaTime * 3f);
+                if(RPM >= redLine)
+                {
+                    RPM = Mathf.Lerp(RPM, redLine - 200f, Time.deltaTime * 20f);
+                }
             }
             else
             {
-                wheelRPM = Mathf.Abs((colliders.RRWheel.rpm + colliders.RLWheel.rpm) / 2f) * gearRatios[currentGear] * differentialRatio;
+                //obtiene la velocidad de las ruedas
+                wheelRPM = Mathf.Abs(GetAllWheelsRPM()) * gearRatios[currentGear] * differentialRatio;
 
-                float engineDesiredRPM = Mathf.Lerp(idleRPM, redLine, Mathf.Pow(gasInput, 2f));
+                //float engineDesiredRPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM, redLine * gasInput) + Random.Range(-50, 50), Time.deltaTime * 3f);
+                RPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM, wheelRPM), Time.deltaTime * 3f);
 
-                float speedFactor = Mathf.Clamp01(speed / 10f);
-                float wheelWeight = Mathf.Lerp(0.8f, 0.3f, speedFactor);
-                wheelWeight = Mathf.Max(wheelWeight, 0.5f * (1f-clutch));
-
-                float rpmBlend = Mathf.Lerp(engineDesiredRPM, wheelRPM, wheelWeight);  
-                RPM = Mathf.Lerp(RPM, Mathf.Max(idleRPM, rpmBlend), Time.deltaTime * 1.1f);
-
-                RPM = Mathf.Clamp(RPM, idleRPM, redLine);
+                //RPM = Mathf.Clamp(RPM, idleRPM, redLine);
 
                 if(RPM >= redLine)
                 {
                     RPM = Mathf.Lerp(RPM, redLine - 200f, Time.deltaTime * 20f);
                 }
 
+                //obtener potencia según las RPM obtenidas
                 currentHp = hpGraph.Evaluate(RPM);
 
-                float engineTorque = currentHp*0.7457f * 9550 / RPM * Mathf.Clamp01(1f - clutch);
+                //convertir de Hp a N/m
+                float engineTorque = currentHp * 0.7457f * 9550 / RPM * Mathf.Clamp01(1f - clutch);
 
+                //cálculo final de potencia entregada
                 torque = engineTorque * gearRatios[currentGear] * differentialRatio;
             }
         }
@@ -181,30 +188,53 @@ public class CarController : MonoBehaviour
         return torque;
     }
 
+    private float GetAllWheelsRPM()
+    {
+        float WheelRpm = 0;
+        switch(driveType)
+        {
+            case DriveType.RWD:
+                WheelRpm = (colliders.RRWheel.rpm + colliders.RLWheel.rpm) / 2f;
+                break;
+            case DriveType.FWD:
+                WheelRpm = (colliders.FRWheel.rpm + colliders.FLWheel.rpm) / 2f;
+                break;
+            case DriveType.AWD:
+            WheelRpm = (colliders.RRWheel.rpm + colliders.RLWheel.rpm + colliders.FRWheel.rpm + colliders.FLWheel.rpm) / 4f;
+            break;
+        }
+
+        return WheelRpm;
+    }
+
+    //aplicar fuerza a las ruedas según el tipo de transmisión
     private void ApplyEngineForce()
     {
-        float totalForce = CalculateTorque() * gasInput;
+        float totalForce = CalculateTorque();
         switch (driveType)
         {
             case DriveType.RWD:
-                colliders.RLWheel.motorTorque = totalForce/2;
-                colliders.RRWheel.motorTorque = totalForce/2;
+                colliders.RLWheel.motorTorque = totalForce / 2 * gasInput;
+                colliders.RRWheel.motorTorque = totalForce / 2 * gasInput;
                 break;
             case DriveType.FWD:
-                colliders.FLWheel.motorTorque = totalForce/2;
-                colliders.FRWheel.motorTorque = totalForce/2;
+                colliders.FLWheel.motorTorque = totalForce / 2 * gasInput;
+                colliders.FRWheel.motorTorque = totalForce / 2 * gasInput;
                 break;
             case DriveType.AWD:
-                colliders.RLWheel.motorTorque = totalForce/4;
-                colliders.RRWheel.motorTorque = totalForce/4;
-                colliders.FLWheel.motorTorque = totalForce/4;
-                colliders.FRWheel.motorTorque = totalForce/4;
+                colliders.RLWheel.motorTorque = totalForce / 4 * gasInput;
+                colliders.RRWheel.motorTorque = totalForce / 4 * gasInput;
+                colliders.FLWheel.motorTorque = totalForce / 4 * gasInput;
+                colliders.FRWheel.motorTorque = totalForce / 4 * gasInput;
                 break;
         }
     }
 
+    //aplicar fuerza de frenado según input y brakeBias(balance de frenado)
     private void ApplyBraking()
     {
+        
+
         colliders.FLWheel.brakeTorque = brakeInput * brakeForce * 0.72f;
         colliders.FRWheel.brakeTorque = brakeInput * brakeForce * 0.72f;
         colliders.RLWheel.brakeTorque = brakeInput * brakeForce * 0.28f;
@@ -219,6 +249,7 @@ public class CarController : MonoBehaviour
         colliders.FRWheel.steerAngle = Mathf.Lerp(colliders.FRWheel.steerAngle, steeringAngle, 5f * Time.deltaTime);
     }
 
+    //actualiza posición y rotación de las ruedas según wheel collider
     private void UpdateWheel(WheelCollider wheelCollider, MeshRenderer wheelMesh, ParticleSystem wheelSmoke, TrailRenderer trailRenderer)
     {
         carFX.UpdateWheelParticles(wheelCollider, wheelSmoke, trailRenderer, speed);
@@ -241,12 +272,6 @@ public class CarController : MonoBehaviour
     private void OnDestroy()
     {
         Instance = null;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position + transform.rotation * centerOfMass, .1f);
     }
 
     IEnumerator ChangeGear(int gearChange)
@@ -281,12 +306,6 @@ public class CarController : MonoBehaviour
         }
         if(gearState!=GearState.Neutral)
         gearState = GearState.Running;
-    }
-
-
-    public float getSpeedRatio()
-    {
-        return Mathf.Sign(speed);
     }
 }
 
